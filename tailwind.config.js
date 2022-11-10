51092a9aa1883616b68e45487e323d4a26d35f5c
quintin156 <99997788+quintin156@users.noreply.github.com>
2022-11-09 23:28:25 -0500

    first section and nav completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html"
-  ],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-red": "hsl(5, 85%, 63%)",
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "off-white": "hsl(36, 100%, 99%)",
+        "grayish-blue": "hsl(233, 8%, 79%)",
+        "dark-grayish-blue": "hsl(236, 13%, 42%)",
+        "very-dark-blue": "hsl(240, 100%, 5%)",
+      },
+    },
   },
   plugins: [],
-}
+};
